4fa896be4bce1afa1873dab15d454cf24724dc0a
Esteban Joel <119338357+Scarface974@users.noreply.github.com>
2024-04-28 17:39:56 -0300

    a

---
+++ inicio.php
@@ -0,0 +1,202 @@
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <!-- Enlace al archivo CSS de Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap">
+    <link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet">
+
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bungee+Spice&family=Honk:SHLN@50.2&display=swap" rel="stylesheet">
+    <!-- Estilos personalizados -->
+    <style>
+        /* Estilo predeterminado para la fuente en pantallas grandes */
+.navbar-nav .nav-link {
+    font-size: 2.0vw; /* Tamaño de fuente para pantallas grandes */
+}
+
+/* Media query para pantallas más pequeñas, como las de dispositivos móviles */
+@media (max-width: 768px) {
+    .navbar-nav .nav-link {
+        font-size: 5.6vw; /* Tamaño de fuente para pantallas más pequeñas */
+    }
+}
+    </style>
+
+<style>
+/* Estilo base para dispositivos grandes FOOTER */
+.texto-grande {
+    font-size: 28px; /* Tamaño más grande para pantallas grandes */
+}
+
+/* Media query para dispositivos más pequeños FOOTER */
+@media (max-width: 768px) {
+    .texto-grande {
+        font-size: 18px; /* Tamaño más pequeño para pantallas pequeñas */
+    }
+}
+</style>
+
+<style>
+/* Estilo base para dispositivos grandes FOOTER 2*/
+.texto-grande2 {
+    font-size: 40px; /* Tamaño más grande para pantallas grandes */
+}
+
+/* Media query para dispositivos más pequeños FOOTER 2*/
+@media (max-width: 768px) {
+    .texto-grande2 {
+        font-size: 20px; /* Tamaño más pequeño para pantallas pequeñas */
+    }
+}
+</style>
+
+ <!-- Estilo  Fuente para Footer -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Bangers&family=Bungee+Inline&family=Bungee+Spice&family=Concert+One&family=Fascinate+Inline&family=Fredericka+the+Great&family=Frijole&family=Honk&family=Kavoon&family=Luckiest+Guy&family=Rampart+One&family=Rubik+Doodle+Shadow&family=Shojumaru&display=swap" rel="stylesheet">
+ <!---------------------------------------->
+
+
+ <!-- Estilo  Fuente para Footer2 -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Bangers&family=Bungee+Inline&family=Bungee+Spice&family=Concert+One&family=Fascinate+Inline&family=Fredericka+the+Great&family=Frijole&family=Honk&family=Kavoon&family=Luckiest+Guy&family=Passion+One:wght@400;700;900&family=Rampart+One&family=Rubik+Doodle+Shadow&family=Shojumaru&display=swap" rel="stylesheet">
+ <!---------------------------------------->
+
+</head>
+<body style="background-image: url('img/wp9684212-anime-compilation-wallpapers.jpg'); background-size: cover; background-position: center;">
+    <!-- Navbar con menú hamburguesa -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="background-image: url('img/14622639415_5a7908cd34_o.jpg'); background-size: cover; background-position: center; border-bottom: 1px solid rgba(255, 255, 255, 0.1); box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.5);">
+    <div class="container-fluid">
+        <!-- Botón para el menú hamburguesa -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" style="font-size: 3vw;">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <!-- Nombre o logo de tu aplicación -->
+        <img src="img/Drew_Feig-removebg-preview.png" alt="logo" class="logo img-fluid" style="width: 140px; height: auto; margin-top: 20px; margin-left: 4px;">
+        <!-- Menú de navegación -->
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav w-100 text-center">
+                <li class="nav-item">
+                    <a class="nav-link honk-font" href="inicio.php" style="color: white; text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000; font-family: 'Honk', system-ui; font-weight: 400; font-style: normal; font-variation-settings: 'MORF' 15, 'SHLN' 50.2;">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="eventos.php" style="color: white; text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000; font-family: 'Honk', system-ui; font-weight: 400; font-style: normal; font-variation-settings: 'MORF' 15, 'SHLN' 50.2;">Eventos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="stands.php" style="color: white; text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000; font-family: 'Honk', system-ui; font-weight: 400; font-style: normal; font-variation-settings: 'MORF' 15, 'SHLN' 50.2;">Stands</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+
+
+
+
+
+
+
+<!-- Titulo / Anuncio -->
+<div class="jumbotron p-3 mb-5" style="background-image: url(''); background-size: auto 100%; background-position: center; text-align: center; text-shadow: -1px -1px 0 #FF0000, 1px -1px 0 #FF0000, -1px 1px 0 #FF0000, 6px 6px 0 #FF0000;">
+    <h1 class="display-3 fs-md-4 fs-lg-5 fs-xl-6" style="color: white; font-family: 'Bangers', cursive, sans-serif; font-size: 7vw; margin-top: 2vw; max-width: 100%;">Próximos Eventos !!!</h1>
+</div>
+
+
+
+
+
+<!-- Carrusel de imágenes -->
+<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel" style="max-width: 800px; margin: auto;">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="img/Carrusel2.png" class="d-block w-100" style="max-height: 900px;" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="img/Carrusel3.png" class="d-block w-100" style="max-height: 900px;" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="img/Carrusel5.png" class="d-block w-100" style="max-height: 900px;" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="img/Carrusel4.png" class="d-block w-100" style="max-height: 900px;" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="img/Carrusel1.png" class="d-block w-100" style="max-height: 900px;" alt="...">
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Anterior</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Siguiente</span>
+    </button>
+</div>
+
+<!-- Línea de separación transparente -->
+<div style="border-top: 1px solid rgba(0, 0, 0, 0.0); margin: 20px auto;"></div>
+
+
+    <!-- Incluye el archivo JavaScript de Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+
+<footer style="background-image: url('ruta/a/la/imagen.jpg'); background-size: cover; background-position: center; padding: 5vw; text-align: center;">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-15" style="background-color: rgba(0, 0, 0, 0.5); padding: 5vw; border-radius: 6%;">
+                <div class="text-center">
+                    <h4 class="texto-grande" style="color: white; text-shadow: -5px -5px 0 #000, 2px -2px 0 #000, -2px 2px 0 #000; font-family: 'Shojumaru', system-ui; font-weight: 400; font-style: normal;">¿Queres aparecer en Eventos Friki como Stand o Evento?</h4>
+                    <p class="texto-grande2" style="color: white; text-shadow: -5px -5px 0 #000, 2px -2px 0 #000, -2px 2px 0 #000; font-family: 'Passion One', sans-serif; font-weight: 400; font-style: normal;">
+                        Escribinos a nuestras Redes:
+                    </p>
+                    <!-- Botón de WhatsApp -->
+                    <a href="https://wa.me/3794774418" target="_blank" style="margin-right: 20px;">
+                        <img src="img/wasap.png" alt="WhatsApp" style="background-image: url('img/anaranjado.jpg'); margin-top: -10px; width: 10vw; max-width: 80px; height: auto; background-size: cover; background-position: center; padding: -2vw; border-radius: 50%;">
+                    </a>
+
+                    <!-- Botón de Instagram -->
+                    <a href="https://www.instagram.com/expo.otaku/" target="_blank">
+                        <img src="img/instagram.png" alt="Instagram" style="background-image: url('img/anaranjado.jpg'); margin-top: -10px; width: 10vw; max-width: 80px; height: auto; background-size: cover; background-position: center; padding: -2vw; border-radius: 50%;">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
+
+
+
+
+
+
+
+
+
+
+</html>
