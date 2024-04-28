80713d5c90c7a494f6e2009a921aa51db4139046
Esteban Joel <119338357+Scarface974@users.noreply.github.com>
2024-04-28 15:23:36 -0300

    a

--- firebase.js
+++ firebase.js
@@ -1,16 +1,7 @@
-// Configuración de tu aplicación Firebase
-  const firebaseConfig = {
-    // Tu configuración de Firebase aquí
-    apiKey: "AIzaSyC4gn1aj1pAh66CsQNn1uNVcYxw5ODt14Y",
-    authDomain: "eventos-friki.firebaseapp.com",
-    projectId: "eventos-friki",
-    storageBucket: "eventos-friki.appspot.com",
-    messagingSenderId: "224267774326",
-    appId: "1:224267774326:web:a1c4b6c9d839eb156b3257",
-    measurementId: "G-NET35J0HV6"
-  };
-
-  // Inicializa Firebase
-  firebase.initializeApp(firebaseConfig);
-  const analytics = firebase.analytics();
-
+{
+    "hosting"; {
+      "site"= "eventosfriki",
+      "public"= "public"
+    }
+  }
+  
